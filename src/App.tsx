d56bbf7cfb85708b1ed966072db0bae09d3eb2a9
Rikelmy Lopes <rikelmylopes899@gmail.com>
2025-08-20 10:10:39 -0300

    feat: melhoria de interface e adicionado animação de loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
+import "./css/loading-animation.css";
 import { useEffect, useState } from "react";
 import { pathExists } from "./utils/fsUtils";
 import { listen } from "@tauri-apps/api/event";
@@ -9,60 +10,66 @@ function App() {
   const [log, setLog] = useState("");
   const [origem, setOrigem] = useState("");
   const [_destino, setDestino] = useState("");
-  
+
+  function testeOnly() {
+    setIsRunning(true);
+    setTimeout(() => {
+      setIsRunning(false);
+    }, 5000);
+  }
 
   async function execute() {
+    /* testeOnly(); */
     if (origem.length === 0) return;
     if (isRunning) return;
 
     if (!(await pathExists(origem))) {
-        setLog('Caminho de origem não encontrado');
-        return;
+      setLog("Caminho de origem não encontrado");
+      return;
     }
-    setIsRunning(true)
+    setIsRunning(true);
     invoke("run_command", { origem });
 
     listen("command-complete", ({ payload }) => {
       setLog(payload as string);
       setIsRunning(false);
-    })
+    });
   }
 
   useEffect(() => {
     if (log.length !== 0) {
       setTimeout(() => {
-        setLog("")
-      }, 3000)
+        setLog("");
+      }, 3000);
     }
-  }, [log])
+  }, [log]);
 
   return (
     <main className="container">
-      <input 
-        type="text" 
-        name="" 
-        id="" 
-        placeholder="Origem" 
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Origem"
         onChange={({ target }) => setOrigem(target.value)}
         disabled={isRunning}
-        />
-      <input 
-        type="text" 
-        name="" 
-        id="" 
-        placeholder="Destino" 
+      />
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Destino"
         onChange={({ target }) => setDestino(target.value)}
         disabled={isRunning}
-        />
-      <button
-        disabled={isRunning}
-        onClick={execute}>
-        Executar
-      </button>
-      <div>
-    {
-      log
-    }
+      />
+      <div className="container-button">
+        <button disabled={isRunning} onClick={execute}>
+          Executar
+        </button>
+        <div>
+          {log} {isRunning ? "Processo inicializado, aguarde!" : ""}
+        </div>
+        <div className={isRunning ? "loader" : ""}></div>
       </div>
     </main>
   );
