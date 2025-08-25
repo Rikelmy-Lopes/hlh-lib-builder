8180a792f528fe76e06c0eab6aaa2016007e89dc
Rikelmy Lopes <rikelmylopes899@gmail.com>
2025-08-25 14:26:44 -0300

    fix: pega os erros da funcao join na função copyBuildFileToDestination

--- src/utils/fsUtils.ts
+++ src/utils/fsUtils.ts
@@ -5,9 +5,9 @@ import { error } from "@tauri-apps/plugin-log";
 
 export async function copyBuildFileToDestination(sourceProject: string, targetProject: string) {
   const jarFileName = "SIGP_INT.jar";
-  const fullSourceProjectPath = await join(sourceProject, "dist", jarFileName);
-  const fullTargetProjectPath = await join(targetProject, DESTINATION_LIB_PATH, jarFileName);
   try {
+    const fullSourceProjectPath = await join(sourceProject, "dist", jarFileName);
+    const fullTargetProjectPath = await join(targetProject, DESTINATION_LIB_PATH, jarFileName);
     await copyFile(fullSourceProjectPath, fullTargetProjectPath);
     return true;
   } catch (e) {
