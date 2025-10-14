af544304d3c6a06a197dd0803a43355b5fe9a7ed
Rikelmy Lopes <rikelmylopes899@gmail.com>
2025-10-14 09:32:15 -0300

    refac: colocado os eventos em caixa alta

--- src-tauri/src/config/constants.rs
+++ src-tauri/src/config/constants.rs
@@ -1,12 +1,14 @@
-pub const ANT_EVENT_COMPLETE_SUCCESSFUL: &str = "ant_complete_successful";
-pub const ANT_EVENT_COMPLETE_WITH_ERROR: &str = "ant_complete_with_error";
-pub const _7ZIP_EVENT_COMPLETE_SUCCESSFUL: &str = "7zip_complete_successful";
-pub const _7ZIP_EVENT_COMPLETE_WITH_ERROR: &str = "7zip_complete_with_error";
-pub const EVENT_RESOURCE_ERROR: &str = "resource_error";
-pub const EVENT_CANCEL_SENT: &str = "cancel_sent";
-pub const EVENT_CANCEL_RECEIVED: &str = "cancel_received";
+pub const ANT_EVENT_COMPLETE_SUCCESSFUL: &str = "ANT_COMPLETE_SUCCESSFUL";
+pub const ANT_EVENT_COMPLETE_WITH_ERROR: &str = "ANT_COMPLETE_WITH_ERROR";
+pub const _7ZIP_EVENT_COMPLETE_SUCCESSFUL: &str = "7ZIP_COMPLETE_SUCCESSFUL";
+pub const _7ZIP_EVENT_COMPLETE_WITH_ERROR: &str = "7ZIP_COMPLETE_WITH_ERROR";
+pub const EVENT_RESOURCE_ERROR: &str = "RESOURCE_ERROR";
+pub const EVENT_CANCEL_SENT: &str = "CANCEL_SENT";
+pub const EVENT_CANCEL_RECEIVED: &str = "CANCEL_RECEIVED";
+
 pub const ANT_RESOURCE_PATH: &str = "resources/apache-ant/bin/";
 pub const SEVEN_ZIP_RESOURCE_PATH: &str = "resources/7zip/7z.exe";
 pub const ANT_COMMAND: &str = if cfg!(windows) { "ant.bat" } else { "ant" };
 pub const BUILD_EXTENSION: &str = "build.xml";
+
 pub const CREATE_NO_WINDOW_FLAG: u32 = 0x08000000;
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,15 +1,17 @@
-export const ANT_EVENT_COMPLETE_SUCCESSFUL = "ant_complete_successful";
-export const ANT_EVENT_COMPLETE_WITH_ERROR = "ant_complete_with_error";
-export const _7ZIP_EVENT_COMPLETE_SUCCESSFUL = "7zip_complete_successful";
-export const _7ZIP_EVENT_COMPLETE_WITH_ERROR = "7zip_complete_with_error";
-export const EVENT_RESOURCE_ERROR = "resource_error";
-export const EVENT_CANCEL_SENT = "cancel_sent";
-export const EVENT_CANCEL_RECEIVED = "cancel_received";
-export const EVENT_REUSE_FILE = "event_reuse_file";
+export const ANT_EVENT_COMPLETE_SUCCESSFUL = "ANT_COMPLETE_SUCCESSFUL";
+export const ANT_EVENT_COMPLETE_WITH_ERROR = "ANT_COMPLETE_WITH_ERROR";
+export const _7ZIP_EVENT_COMPLETE_SUCCESSFUL = "7ZIP_COMPLETE_SUCCESSFUL";
+export const _7ZIP_EVENT_COMPLETE_WITH_ERROR = "7ZIP_COMPLETE_WITH_ERROR";
+export const EVENT_RESOURCE_ERROR = "RESOURCE_ERROR";
+export const EVENT_CANCEL_SENT = "CANCEL_SENT";
+export const EVENT_CANCEL_RECEIVED = "CANCEL_RECEIVED";
+export const EVENT_REUSE_FILE = "EVENT_REUSE_FILE";
+
 export const DESTINATION_LIB_PATH = "/src/main/webapp/WEB-INF/lib";
 export const CONFIG_FILE = "config/settings.json";
 export const BUILD_EXTENSION = "build.xml";
 export const JAR_FILE_NAME = "SIGP_INT.jar";
+
 export const ERROR_MESSAGES = Object.freeze({
   BUILD_XML_NOT_FOUND:
     "Arquivo build.xml não encontrado na origem! Verifique o caminho do projeto!",
